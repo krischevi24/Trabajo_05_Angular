57bde56656a95391ffdaf0e28b0bf42e627cbcac
krischevi24 <kevin.morales@alumnos.udg.mx>
2021-11-07 00:00:30 -0600

    Segunda subida

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', score: 20 },
+  { id: 12, name: 'Narco', score: 6  },
+  { id: 13, name: 'Bombasto', score: 9  },
+  { id: 14, name: 'Celeritas', score: 13  },
+  { id: 15, name: 'Magneta', score: 1  },
+  { id: 16, name: 'RubberMan', score: 5  },
+  { id: 17, name: 'Dynama', score: 3  },
+  { id: 18, name: 'Dr IQ', score: 9  },
+  { id: 19, name: 'Magma', score: 21  },
+  { id: 20, name: 'Tornado', score: 10  }
+];
